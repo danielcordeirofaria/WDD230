30056883629c7934251e7184f87a7ab248cffdf7
danielcordeirofaria <danielcordeirofaria@yahoo.com.br>
2024-03-08 09:20:46 -0300

    dark mode done

---
+++ scripts/darkModeButton.js
@@ -0,0 +1,20 @@
+const modeButton = document.querySelector("#mode");
+const pageInformation = document.querySelector('.pageInformation')
+const lessonList = document.querySelector('.lessonList')
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		pageInformation.style.color = "#fff"
+		lessonList.style.color = "#fff"
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		lessonList.style.color = "var(--blue)"
+		pageInformation.style.color = "var(--blue)"
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
