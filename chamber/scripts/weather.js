1c132a941a9cf34f29b2ffe146390ecb749cdb57
danielcordeirofaria <danielcordeirofaria@yahoo.com.br>
2024-04-03 10:47:44 -0300

    adicionando a previsão dos próximos dias

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,69 +1,33 @@
 
-// const lat = -25.49;
-// const long = -49.34;
-// const key = "3fd567ec38f593abe6ddc29dd6159dcd";
-
-// const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=${key}`;
-
-// async function apiFetch() {
-//     try {
-//         let response = await fetch(url);
-//         if (response.ok) {
-//             const data = await response.json();
-//             displayResults(data);
-//         } else {
-//             throw new Error(await response.text());
-//         }
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// function displayResults(data) {
-//     const weatherImg = document.querySelector('.weatherImg');
-//     const temperature = document.querySelector('.temperature');
-//     const weatherDescription = document.querySelector('.weatherDescription');
-//     const humidityNumber = document.querySelector('.humidityNumber');
-//     const windSpeed = data.wind.speed
-//     console.log(windSpeed)
-
-//     weatherImg.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
-//     weatherImg.setAttribute('alt', 'Weather Icon');
-//     weatherImg.setAttribute('width', 100);
-
-//     temperature.innerHTML = `${data.main.temp.toFixed(1)}&deg;F`;
-
-//     weatherDescription.innerHTML = `${capitalizeFirstLetter(data.weather[0].description)}`
-
-//     humidityNumber.innerHTML = `${data.main.humidity}%`
-
-
-//     const windChillParameter = document.querySelector(".windChillParameter");
-
-//     windChillParameter.textContent = calculateWindChill(temperature.innerHTML, windSpeed);
-    
-    
-// }
-
-
-// function capitalizeFirstLetter(string) {
-//     return string.charAt(0).toUpperCase() + string.slice(1);
-// }
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     apiFetch();
-// });
-
-
+const lat = -25.49;
+const long = -49.34;
 const key = "3fd567ec38f593abe6ddc29dd6159dcd";
 
-async function apiFetch(lat, long) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=${key}`;
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=${key}`;
+
+const urlForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=-25.495342&lon=-49.311614&units=imperial&appid=3fd567ec38f593abe6ddc29dd6159dcd`
+
+async function apiFetch() {
     try {
         let response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
             displayResults(data);
+            fetchForecast();
+        } else {
+            throw new Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function fetchForecast() {
+    try {
+        let response = await fetch(urlForecast);
+        if (response.ok) {
+            const dataForecast = await response.json();
+            calculateForecast(dataForecast);
         } else {
             throw new Error(await response.text());
         }
@@ -76,8 +40,7 @@ function displayResults(data) {
     const weatherImg = document.querySelector('.weatherImg');
     const temperature = document.querySelector('.temperature');
     const weatherDescription = document.querySelector('.weatherDescription');
-    const humidityNumber = document.querySelector('.humidityNumber');
-    const windSpeed = data.wind.speed;
+
 
     weatherImg.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
     weatherImg.setAttribute('alt', 'Weather Icon');
@@ -85,44 +48,80 @@ function displayResults(data) {
 
     temperature.innerHTML = `${data.main.temp.toFixed(1)}&deg;F`;
 
-    weatherDescription.innerHTML = `${capitalizeFirstLetter(data.weather[0].description)}`;
-
-    humidityNumber.innerHTML = `${data.main.humidity}%`;
-
-    const windChillParameter = document.querySelector(".windChillParameter");
-
-    windChillParameter.textContent = calculateWindChill(temperature.innerHTML, windSpeed);
+    weatherDescription.innerHTML = `${capitalizeFirstLetter(data.weather[0].description)}`  
+    
 }
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function calculateWindChill(temperature, windSpeed) {
-    // Implement your wind chill calculation logic here
+
+
+function calculateForecast(data) {
+    // Obter a data de amanhã e do próximo dia
+    let tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+
+    let nextDay = new Date();
+    nextDay.setDate(nextDay.getDate() + 2);
+
+    // Formatar as datas para o formato YYYY-MM-DD
+    let tomorrowFormatted = tomorrow.toISOString().split('T')[0];
+    let nextDayFormatted = nextDay.toISOString().split('T')[0];
+
+    // Filtrar os itens da lista que têm as datas de amanhã e do próximo dia
+    let tomorrowItems = data.list.filter(item => item.dt_txt.includes(tomorrowFormatted));
+    let nextDayItems = data.list.filter(item => item.dt_txt.includes(nextDayFormatted));
+
+    // Encontrar a menor e a maior temperatura de amanhã
+    let minTemperatureTomorrow = Math.min(...tomorrowItems.map(item => item.main.temp_min));
+    let maxTemperatureTomorrow = Math.max(...tomorrowItems.map(item => item.main.temp_max));
+
+    // Encontrar a menor e a maior temperatura do próximo dia
+    let minTemperatureNextDay = Math.min(...nextDayItems.map(item => item.main.temp_min));
+    let maxTemperatureNextDay = Math.max(...nextDayItems.map(item => item.main.temp_max));
+
+    // Acessar os elementos do DOM
+    const dataTomorrow = document.querySelector('.dataTomorrow');
+    const minTempTomorrow = document.querySelector('.minTempTomorrow');
+    const maxTempTomorrow = document.querySelector('.maxTempTomorrow');
+
+    const dataNextDay = document.querySelector('.dataNextDay');
+    const minTempNextDay = document.querySelector('.minTempNextDay');
+    const maxTempNextDay = document.querySelector('.maxTempNextDay');
+
+    function formatDateAmericanStyle(date) {
+        const year = date.getFullYear();
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const day = date.getDate().toString().padStart(2, '0');
+        return `${month}/${day}/${year}`;
+    }
+
+    let tomorrowFormattedNewMode = formatDateAmericanStyle(tomorrow);
+    tomorrowFormattedNewMode = "Date: " + tomorrowFormattedNewMode
+
+    let nextDayFormattedNewMode = formatDateAmericanStyle(nextDay);
+    nextDayFormattedNewMode =  "Date: " + nextDayFormattedNewMode
+    
+    let minTempTomorrowformatted = `Min. Temp.: ${minTemperatureTomorrow}&deg;F`
+    let maxTemperatureTomorrowFormatted = `Max. Temp.:  ${maxTemperatureTomorrow}&deg;F`
+
+    let minTemperatureNextDayformatted = `Min. Temp.: ${minTemperatureTomorrow}&deg;F`
+    let maxTemperatureNextDayFormatted = `Max. Temp.:  ${maxTemperatureTomorrow}&deg;F`
+
+
+    // Definir o conteúdo dos elementos HTML
+    dataTomorrow.innerHTML = tomorrowFormattedNewMode;
+    minTempTomorrow.innerHTML = minTempTomorrowformatted;
+    maxTempTomorrow.innerHTML = maxTemperatureTomorrowFormatted;
+
+    dataNextDay.innerHTML = nextDayFormattedNewMode;
+    minTempNextDay.innerHTML = minTemperatureNextDayformatted;
+    maxTempNextDay.innerHTML = maxTemperatureNextDayFormatted;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            const lat = position.coords.latitude;
-            const long = position.coords.longitude;
-            apiFetch(lat, long);
-        }, function (error) {
-            if (error.code === error.PERMISSION_DENIED) {
-                alert("You denied the geo-location request. The weather that will be rendered will be the default location.");
-                const lat = -25.49;
-                const long = -49.34;
-                apiFetch(lat, long);
-            } else {
-                alert("An error occurred while trying to get your location.");
-            }
-        });
-    } else {
-        alert("Geolocation is not supported by this browser. The weather that will be rendered will be the default location.");
-        const lat = -25.49;
-        const long = -49.34;
-        apiFetch(lat, long);
-    }
+    apiFetch();
+    fetchForecast();
 });
-
